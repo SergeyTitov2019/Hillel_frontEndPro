2c24b5e188133cb91ee16336a823753b41cd1563
Sergey Titov <sergeyntitov@gmail.com>
2021-10-13 22:59:56 +0300

    lesson 05, final

--- lesson_05/variant_02/js/app.js
+++ lesson_05/variant_02/js/app.js
@@ -5,13 +5,12 @@ const discountValueToCalculate = 1 - discountValue / 100;
 const selectedCategory = document.getElementById("select");
 const selectedItem = document.getElementById("input1");
 const selectedAmount = document.getElementById("input2");
-const formButton = document.querySelector('#btn-form')
 
-let productNumber;
-let productAmount;
+let productNumber = 0;
+let productAmount = 0;
 const selectedProduct = products[productNumber-1];
 const totalPrice = productAmount * selectedProduct.price;
-formButton.addEventListener('click', btnClick)
+
 getItem()
 getAmount()
 getPrice()
---
+++ lesson_05/variant_02/js/index.js
@@ -0,0 +1,16 @@
+// const priceToGetDiscount = 5000;
+// const discountValue = 5;
+// const discountValueToCalculate = 1 - discountValue / 100;
+// let productCategory;
+// let productNumber;
+// let productAmount;
+// showProductList()
+// getCategory()
+// getProductNumber()
+// const selectedProduct = products[productNumber-1];
+// getAmount()
+// const totalPrice = productAmount * selectedProduct.price;
+// getNotification(priceObj)
+// getDiscount()
+
+
